d1568ddc86da7f7e350ec8bd7e2dcf4f0b8ab67b
rtaufikaditiya <rtaufikaditiya938@gmail.com>
2025-11-07 22:16:19 +0700

    Add files via upload

---
+++ myfirst.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // a PREPROCESSOR directive
+int main() // function header
+{ // start of function body
+    using namespace std; // make definitions visible
+    cout << "Come up and C++ me some time."; // message
+    cout << endl; // start a new line
+    cout << "You won't regret it!" << endl; // more output
+
+    cout << "Rahmat  Taufik Aditiya 255440006";
+    return 0; // terminate main()
+} // end of function body}
